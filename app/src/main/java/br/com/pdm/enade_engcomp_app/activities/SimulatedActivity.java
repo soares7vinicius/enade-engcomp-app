492fadb0c0b5bd8215c674e0bd337db0bea6f26e
franciellenunes <franciellesnunes@gmail.com>
2018-06-30 17:11:17 -0300

    Creation of the toolbar and simulated activity

---
+++ app/src/main/java/br/com/pdm/enade_engcomp_app/activities/SimulatedActivity.java
@@ -0,0 +1,41 @@
+package br.com.pdm.enade_engcomp_app.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.pdm.enade_engcomp_app.R;
+
+public class SimulatedActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulated);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    Intent intent = new Intent(SimulatedActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+
+    }
+}
