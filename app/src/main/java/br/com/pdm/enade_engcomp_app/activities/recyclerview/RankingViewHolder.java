d458c876b76ef3f36a7910178262166bfd58d282
Marco Antônio <marcoantonioafernandes@gmail.com>
2018-07-02 18:21:06 -0300

    Creation of the ranking

--- app/src/main/java/br/com/pdm/enade_engcomp_app/activities/recyclerview/RankingViewHolder.java
+++ app/src/main/java/br/com/pdm/enade_engcomp_app/activities/recyclerview/RankingViewHolder.java
@@ -3,8 +3,13 @@ package br.com.pdm.enade_engcomp_app.activities.recyclerview;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.RequestManager;
 
 import br.com.pdm.enade_engcomp_app.R;
 import br.com.pdm.enade_engcomp_app.model.User;
@@ -23,18 +28,28 @@ public class RankingViewHolder extends RecyclerView.ViewHolder implements View.O
         this.rankingCard.setOnClickListener(this);
     }
 
-    public void bind(User user){
+    public void bind(User user, int position){
         this.user = user;
         TextView userName = (TextView) this.rankingCard.getChildAt(0).findViewById(R.id.userName);
         TextView userResult = (TextView) this.rankingCard.getChildAt(0).findViewById(R.id.userResult);
-        userName.setText("Simulado " + this.user.getId());
-        //DESCOMENTAR
-        //userResult.setText(this.user.getPoints());
+        ImageView imgFirstPlace = (ImageView) this.rankingCard.findViewById(R.id.imgUser);
+
+        String image = user.getPhoto();
+
+        // Create glide request manager
+        RequestManager requestManager = Glide.with(this.rankingCard);
+        // Create request builder and load image.
+        RequestBuilder requestBuilder = requestManager.load(image);
+        // Show image into target imageview.
+        requestBuilder.into(imgFirstPlace);
+
+        userName.setText(this.user.getName());
+        userResult.setText(this.user.getPoints() + "");
     }
 
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(v.getContext(), "clicou", Toast.LENGTH_LONG).show();
+        //Toast.makeText(v.getContext(), "clicou", Toast.LENGTH_LONG).show();
     }
 }
