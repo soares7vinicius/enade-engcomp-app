7c73bfa70720d35d1a97174970be35b1835afa63
franciellenunes <franciellesnunes@gmail.com>
2018-07-01 22:36:21 -0300

    Adjustments in simulated activity and update of sdk version

--- app/src/main/java/br/com/pdm/enade_engcomp_app/activities/CorrectedSimulationActivity.java
+++ app/src/main/java/br/com/pdm/enade_engcomp_app/activities/CorrectedSimulationActivity.java
@@ -2,14 +2,30 @@ package br.com.pdm.enade_engcomp_app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.pdm.enade_engcomp_app.R;
+import br.com.pdm.enade_engcomp_app.model.Question;
 
 public class CorrectedSimulationActivity extends AppCompatActivity {
+
+    private List<Boolean> correct_questions;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corrected_simulation);
+
+        ArrayList<Boolean> correct_questions = (ArrayList<Boolean>) getIntent().getSerializableExtra("CORRECT_QUESTIONS");
+        int qtt_corrects = getIntent().getIntExtra("QTT_CORRECTS", 0);
+        int total_questions = getIntent().getIntExtra("TOTAL_QUESTIONS", 0);
+
+        //ADD TOOLBAR
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
